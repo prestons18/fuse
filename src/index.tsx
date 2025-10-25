98e9436f85b91a411955fb44a5dd82cceb86b5c2
Preston Arnold <me@prestonarnold.uk>
2025-10-25 19:16:51 +0100

    wrap `count.get()` in a reactive effect

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ function Counter() {
     const count = signal(0);
     return (
         <div>
-            <h1>Count: {count.get()}</h1>
+            <h1>Count: {() => count.get()}</h1>
             <button onClick={() => count.set(count.get() + 1)}>Increment</button>
         </div>
     )
