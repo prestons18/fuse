350f91ee0c7f78e837b580d9c68dbc7a4eaf0293
Preston Arnold <me@prestonarnold.uk>
2025-10-25 19:12:13 +0100

    Add example

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+/** @jsx h */
+import { h, render } from "./fuse/dom";
+import { signal } from "./fuse/reactivity";
+
+function Counter() {
+    const count = signal(0);
+    return (
+        <div>
+            <h1>Count: {count.get()}</h1>
+            <button onClick={() => count.set(count.get() + 1)}>Increment</button>
+        </div>
+    )
+}
+
+render(<Counter />, document.body);
